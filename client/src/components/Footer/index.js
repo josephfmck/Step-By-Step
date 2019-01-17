d1a84c41d17b2b2e3dd479198ba6e90c6b291af2
Micah Walker <minimelon19@gmail.com>
2019-01-16 20:20:47 -0600

    Created 3 Pages

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,11 +3,11 @@ import React from "react";
 function Footer() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary">
-      <a className="navbar-brand" href="/" style={{textAlign: "center"}}>
-        Copyright
-      </a>
-  
-    </nav>
+    <a className="navbar-brand" href="/" style={{textAlign: "center"}}>
+      Copyright
+    </a>
+
+  </nav>
     
   );
 }
