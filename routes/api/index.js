c7f715d35177a3332aa619093fbb64344a79a4b7
Micah Walker <minimelon19@gmail.com>
2019-01-26 12:46:36 -0600

    submit button

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const stepByStepRoutes = require("./stepBySteps");
 
 // Book routes
-router.use("/stepbysteps", stepByStepRoutes);
+router.use("/steps", stepByStepRoutes);
 
 module.exports = router;
